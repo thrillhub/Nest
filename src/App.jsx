aebc5d0a73a8d8a8ebf3f49949fdaa1554628e0d
Aakash Yadav <your-email@example.com>
2024-09-05 18:18:43 +0545

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './Components/header/Header'
-import Footer from './Components/footer/Footer'
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
